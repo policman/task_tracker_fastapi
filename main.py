87d4a3f02a2afd626db1bbb1473a66eff13ff7e6
Gennadiy Samoylov <samoylowfgg@gmail.com>
2026-04-03 17:41:50 +0300

    dev: add base router in main

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import uvicorn
 from fastapi import FastAPI
 
+import core.config
+from api import router as api_router
 app = FastAPI()
+app.include_router(
+    api_router,
+    prefix=core.config.settings.api
+)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
