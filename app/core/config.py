7091224e13bfd9451082a45ba09b23f7c88d79ba
Gennadiy Samoylov <samoylowfgg@gmail.com>
2026-04-15 03:21:57 +0300

    dev: add config info for db

--- app/core/config.py
+++ app/core/config.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel, PostgresDsn
+from pydantic_core import MultiHostUrl
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class RunConfig(BaseModel):
@@ -8,14 +9,28 @@ class RunConfig(BaseModel):
 class ApiPrefix(BaseModel):
     prefix: str = "/api/v1"
 
-class DatabaseConfig(BaseModel):
-    url: PostgresDsn
-
 
 class Settings(BaseSettings):
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
-    db: DatabaseConfig
+
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_HOST: str = "localhost"
+    POSTGRES_PORT: int = 5432
+    POSTGRES_DB: str
+
+    @property
+    def database_url(self) -> str:
+        url = MultiHostUrl.build(
+            scheme="postgresql+asyncpg",
+            username=self.POSTGRES_USER,
+            password=self.POSTGRES_PASSWORD,
+            host=self.POSTGRES_HOST,
+            port=self.POSTGRES_PORT,
+            path=self.POSTGRES_DB,
+        )
+        return str(url)
 
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
